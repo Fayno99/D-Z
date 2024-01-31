949b4d3763757c82bf44432d34c25fe20cd9161d
Fayno99 <Fayno99@gmail.com>
2024-01-31 21:02:04 +0100

    dz_lessons 13 add checkbox

--- lessons 13/lessons 13.php
+++ lessons 13/lessons 13.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <?php
 session_start();
 if (!isset($_SESSION['Admin'])) {
@@ -30,106 +29,105 @@ $pdo= null;
             src="https://code.jquery.com/jquery-3.7.1.js"
             integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
             crossorigin="anonymous"></script>
-<script>
-    function BluurWord() {
-        // Get the checkbox
-        var checkBox = document.getElementById("myCheck");
-        // Get the output text
-        var text = document.getElementById("text");
-          // If the checkbox is checked, display the output text
-        let forbiddenWord;
-        if (checkBox.checked == true){
-            text.style.display = "block";
-             forbiddenWord = "забарон";
-        } else {
-            text.style.display = "none";
-             forbiddenWord = "забарона";
-
+    <script>
+        function BluurWord() {
+            var checkBox = document.getElementById("myCheck"); // Get the checkbox
+            let forbiddenWord;  // If the checkbox is checked, display the output text
+            if (checkBox.checked === true) {
+                forbiddenWord = "заборона";
+            } else {
+                forbiddenWord = null;
+            }
+            //  console.log(forbiddenWord)
+            return forbiddenWord
         }
-        console.log(forbiddenWord)
-        return forbiddenWord
+        $(document).ready(function() {  // Завантаження повідомлення при завантаженні сторінки
+            loadMessages();
 
-    }
-    function appendComment(login, timestamp, chat, ID, BluurWord) {
-    let isAdmin = "<?php echo $_SESSION['Admin']; ?>";
-    let del = isAdmin === 'Admin' ? "<a href='#' data-id='" + ID + "' class='delete-button' id='hide-x'>X</a> " :"";
-    let forbid = BluurWord;
-    if (chat.includes(forbid)) {
-        chat = chat.replace(new RegExp(forbid, 'g'), '********');
-    }
-        console.log('2',BluurWord)
-    $("#list-of-messages").append(
-        "<li class='list-group-item'><strong>"+ login + "</strong>"+" "+ timestamp + ": " + chat + " "+ del +" </li>"
-    );
-}
+            $('#myCheck').change(function() {  // Перевірка повідомлення, коли стан чекбоксу змінюється
+                loadMessages();
+            });
 
-$(document).ready(function(){
-        $.ajax({
-            url:'messages.php',
-            method: 'GET',
-            success: function(request){
-               // console.log(request);
-                if (request.data){
-                    request.data.map(function (comment){
-              //    console.log(comment);
-                       appendComment(comment.login, comment.timestamp, comment.chat, comment.ID)
-                    })
+            function loadMessages() {
+                $.ajax({
+                    url: 'messages.php',
+                    method: 'GET',
+                    success: function (request) {
+                        if (request.data) {
+                            let forbiddenWord = BluurWord();
+                            $("#list-of-messages").empty();  // Очистка списока повідомлень
+                            request.data.map(function (comment) {
+                                appendComment(comment.login, comment.timestamp, comment.chat, comment.ID, forbiddenWord)
+                            })
+                        }
+                    }
+                })
+            }
+
+            function appendComment(login, timestamp, chat, ID, forbiddenWord) {
+                let isAdmin = "<?php echo $_SESSION['Admin']; ?>";
+                let del = isAdmin === 'Admin' ? "<a href='#' data-id='" + ID + "' class='delete-button' id='hide-x'>X</a> " : "";
+                if (chat.includes(forbiddenWord)) {
+                    chat = chat.replace(new RegExp(forbiddenWord, 'g'), '********');
                 }
+                $("#list-of-messages").append(
+                    "<li class='list-group-item'><strong>" + login + "</strong>" + " " + timestamp + ": " + chat + " " + del + " </li>"
+                );
             }
-        })
 
-    $("form").submit(function(event){
-          event.preventDefault();
-          let date = new Date();
-          let formattedDate = date.toISOString().slice(0,19).replace("T", " ");
-          let data1 = $(this).serialize();
-          let data2 ={
-              Admin: "<?php echo $_SESSION['Admin']; ?>",
-              data:formattedDate,
-              mes: $(this).find('input[name="mes"]').val()
-              };
-          console.log('Variant 1 :'+ data1)
-          console.log('Variant 2 :')
-          console.log(data2)
-          $.ajax({
-              url:'newMessages.php',
-              method: 'POST',
-              data: data2,
-              success: function(request){
-                  appendComment(data2.Admin,data2.data,data2.mes);
-                  $('form').trigger("reset");
-              }
-          })
-      })
-          //видалення повідомлення
-      $(document).on("click", ".delete-button", function() {
-          let messageId = $(this).data('id'); // витягуємо ID воно повинно бути маленькими літерами
-          let messageElement = $(this).closest('li');
-          $.ajax({
-              url: 'deleteMessage.php',
-              type: 'GET',
-              data: {
-                  'delete_message': messageId
-              },
-              success: function(response) {
-                  messageElement.remove();  // Оновлення  інтерфейсу користувача після видалення
-              }
-          });
-      });
-         //вихід з чату
-    $("button[name='btnOut']").click(function(event){
-        event.preventDefault();
-        $.ajax({
-            url:'logout.php', // URL, який обробляє вихід на сервері
-            method: 'POST',
-            success: function(response){
-                // Перенаправте користувача на сторінку входу після виходу
-               window.location.href = "./Login.php";
-            }
-        })
-    });
-});
-</script>
+            $("form").submit(function(event){
+                event.preventDefault();
+                let date = new Date();
+                let formattedDate = date.toISOString().slice(0,19).replace("T", " ");
+                let data1 = $(this).serialize();
+                let data2 ={
+                    Admin: "<?php echo $_SESSION['Admin']; ?>",
+                    data:formattedDate,
+                    mes: $(this).find('input[name="mes"]').val()
+                };
+                console.log('Variant 1 :'+ data1)
+                console.log('Variant 2 :')
+                console.log(data2)
+                $.ajax({
+                    url:'newMessages.php',
+                    method: 'POST',
+                    data: data2,
+                    success: function(request){
+                        appendComment(data2.Admin,data2.data,data2.mes);
+                        $('form').trigger("reset");
+                    }
+                })
+            })
+            //видалення повідомлення
+            $(document).on("click", ".delete-button", function() {
+                let messageId = $(this).data('id'); // витягуємо ID воно повинно бути маленькими літерами
+                let messageElement = $(this).closest('li');
+                $.ajax({
+                    url: 'deleteMessage.php',
+                    type: 'GET',
+                    data: {
+                        'delete_message': messageId
+                    },
+                    success: function(response) {
+                        messageElement.remove();  // Оновлення  інтерфейсу користувача після видалення
+                    }
+                });
+            });
+            //вихід з чату
+            $("button[name='btnOut']").click(function(event){
+                event.preventDefault();
+                $.ajax({
+                    url:'logout.php', // URL, який обробляє вихід на сервері
+                    method: 'POST',
+                    success: function(response){
+                        // Перенаправте користувача на сторінку входу після виходу
+                        window.location.href = "./Login.php";
+                    }
+                })
+            });
+        });
+
+    </script>
 
 </head>
 <body class="p-3 mb-2  bg-dark">
@@ -140,27 +138,26 @@ $(document).ready(function(){
             <div class="card-header" > Chat</div>
             <ul class="list-group list-group-flush" id="list-of-messages"> </ul>
         </div>
-    <form method="post" id="form">
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Текст повідомлення</label>
-            <input type="text" class="form-control" name="mes" id="exampleInputPassword1">
-        </div>
-        <button type="submit"  name="btn" class="btn btn-primary">Відправити</button>
-        <button type="submit"  name="btnOut" class="btn btn-primary">Вийти</button>
-      </form>
- <br>
+        <form method="post" id="form">
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Текст повідомлення</label>
+                <input type="text" class="form-control" name="mes" id="exampleInputPassword1">
+            </div>
+            <button type="submit"  name="btn" class="btn btn-primary">Відправити</button>
+            <button type="submit"  name="btnOut" class="btn btn-primary">Вийти</button>
+        </form>
+        <br>
     </div>
-        <br><br>
-    <div class="p-4 mb-3 position-center" style="background-color: slategray; border-radius: 20px;border-color: aqua;">
+    <br><br>
+    <div class="p-4 mb-3 position-center" style="background-color: slategray; border-radius: 20px;border-color: aqua;"> Натисніть щоб приховати слово: заборона
         <input type="checkbox" id="myCheck" onclick="BluurWord()">
-        <p id="text" style="display:none">Checkbox is CHECKED!</p>
-          <h2>Користувачі Online
+        <h2>Користувачі Online
             <span class="badge bg-secondary">
    <?php
-        foreach ($getOnlineUsers as $getOnlineUser):?>
+   foreach ($getOnlineUsers as $getOnlineUser):?>
                 <li class="list-group-item"> <?=$getOnlineUser['userOn'] ?>
-                        <?php endforeach;?>
+                    <?php endforeach;?>
                </span></h2>
-          </div>
+    </div>
 </body>
 </html>
