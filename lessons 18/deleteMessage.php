b8e479b95a975283202a12db0b4dcea34c83ba6b
Fayno99 <94063605+Fayno99@users.noreply.github.com>
2024-02-29 13:51:02 +0100

    Delete lessons 18 directory

--- lessons 18/deleteMessage.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once ('db.php');
-$pdo = getPDO();
-
-if (!empty($_GET['delete_message'])){
- //showAndDie($_GET);
-    deleteMessages($pdo, $_GET['delete_message']);
-}
--- lessons 18/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once ('db.php');
-$pdo = getPDO();
-$messages=getAllMessages($pdo);
-//showAndDie($messages);
- $pdo = null;
-
-header('Content-Type: application/json; charset=UTF-8');
-echo json_encode(['data'=>$messages]);
